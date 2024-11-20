0b12fe83642e0a60afb2e5671f0677e6a51c93e5
Donell Oconer <donellpie@gmail.com>
2024-11-20 19:56:56 +0800

    set up radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuePage = () => {
-  return <div>IssuePage</div>;
+  return <Button>Testing</Button>;
 };
 
 export default IssuePage;

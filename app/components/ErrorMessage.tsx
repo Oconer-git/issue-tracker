bff7b2f9c2333296b355cfdddfd5d02ab8afe8b4
Donell Oconer <donellpie@gmail.com>
2024-11-21 14:16:13 +0800

    add loading skeleton on issues table

--- app/components/ErrorMessage.tsx
+++ app/components/ErrorMessage.tsx
@@ -2,6 +2,7 @@ import React, { PropsWithChildren } from "react";
 import { Text } from "@radix-ui/themes";
 
 const ErrorMessage = ({ children }: PropsWithChildren) => {
+  if (!children) return null;
   return (
     <Text color="red" as="p">
       {children}
